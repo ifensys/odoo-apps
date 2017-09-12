ef844f5d307e2ca38a27d7f678da718db46fe67c
ifensys <prabhu2308@gmail.com>
2017-09-12 16:24:42 +0530

    Add banner

--- sale_invoice_gst_india/__openerp__.py
+++ sale_invoice_gst_india/__openerp__.py
@@ -37,6 +37,7 @@
         'report/inherited_sale_report_template.xml',
         'views/menus.xml'
     ],
+    'images': ['static/description/banner.png'],
     'installable': True,
     'auto_install': False,
 }
